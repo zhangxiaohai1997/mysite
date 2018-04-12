f2244cde05a2c6c235876a86e1d138246ee19e27
Zhangzihao <206835638@qq.com>
2018-04-13 00:39:35 +0800

    Started project.

---
+++ polls/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    #pub_date=models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+class MyModel(models.Model):
+    # 文件被传至`MEDIA_ROOT/uploads`目录，MEDIA_ROOT由你在settings文件中设置
+    upload = models.FileField(upload_to='uploads/')
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+
+
+
---
+++ polls/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect,Http404
+from .models import Question,Choice
+from django.urls import reverse
+from django.views import generic
+
+# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context={'latest_question_list':latest_question_list}
+    return render(request,'polls/index.html',context)
+
+def detail(request, question_id):
+    try:
+        question=Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exit")
+    return render(request,'polls/detail.html',{'question':question})
+def results(request, question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
+
+def vote(request, question_id):
+    question=get_object_or_404(Question,pk=question_id)  #如果异常返回404错误
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        #发生choice未找到异常时,重新返回表单页面,并给出提示信息
+        return render(request,'polls/detail.html',{'question':question,'error_message':"You didn't select a choice.",})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        #成功处理数据后,自动跳转到结果页面,防止用户连续多次提交
+        return  HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+
